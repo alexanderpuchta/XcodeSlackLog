60e8d6b487e93fa029857f07bc2587c84c7d7ab6
Alexander Puchta <alexander.puchta@appsfactory.de>
2020-11-12 14:03:35 +0100

    [ADD] functionality of logger [DEL] default test

--- Sources/XcodeSlackLog/XcodeSlackLog.swift
+++ Sources/XcodeSlackLog/XcodeSlackLog.swift
@@ -7,7 +7,39 @@ import Foundation
 
 open class XcodeSlackLog {
     
-    public static func debug(_ msg: String) {
-        print("debug:: \(msg)")
+    /// Output of debug information with filename + linenumber
+    /// - Parameters:
+    ///   - msg: "\(object)"
+    ///   - file: #file or nil
+    ///   - lineNr: #line or nil
+    public static func debug(_ msg: String, file: String? = nil, lineNr: UInt? = nil) {
+        print("debug :: \(msg) :: file :: \(file ?? "") :: lineNr :: \(lineNr ?? 0)")
+    }
+    
+    /// Output of warning information with filename + linenumber
+    /// - Parameters:
+    ///   - msg: "\(object)"
+    ///   - file: #file or nil
+    ///   - lineNr: #line or nil
+    public static func warning(_ msg: String, file: String? = nil, lineNr: UInt? = nil) {
+        print("warning :: \(msg) :: file :: \(file ?? "") :: lineNr :: \(lineNr ?? 0)")
+    }
+    
+    /// Output of error information with filename + linenumber
+    /// - Parameters:
+    ///   - msg: "\(object)"
+    ///   - file: #file or nil
+    ///   - lineNr: #line or nil
+    public static func error(_ msg: String, file: String? = nil, lineNr: UInt? = nil) {
+        print("error :: \(msg) :: file :: \(file ?? "") :: lineNr :: \(lineNr ?? 0)")
+    }
+    
+    /// Output of critical information with filename + linenumber
+    /// - Parameters:
+    ///   - msg: "\(object)"
+    ///   - file: #file or nil
+    ///   - lineNr: #line or nil
+    public static func critical(_ msg: String, file: String? = nil, lineNr: UInt? = nil) {
+        print("critical :: \(msg) :: file :: \(file ?? "") :: lineNr :: \(lineNr ?? 0)")
     }
 }
